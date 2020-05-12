d7dadb341b70f9ae48edb9f02cf2a1d5893aefe2
alfa098 <64955416+alfa098@users.noreply.github.com>
2020-05-12 17:54:42 +0530

    Create revising-aggregations-the-count-function.sql

---
+++ aggregation/revising-aggregations-the-count-function.sql
@@ -0,0 +1,12 @@
+-- Revising Aggregations - The Count Function
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+
+SELECT COUNT(*)
+FROM City
+WHERE Population > 100000;
+
+-- Alternative
+
+SELECT COUNT(Population) 
+FROM City 
+WHERE Population > 100000;
